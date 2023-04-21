7e27a8da984c132d5cff2b775d8114961fde13d2
hari66mech <harikrishna.manokaran@atmecs.com>
2023-04-21 22:29:21 +0530

    Build custom component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Expanses from "./components/Expanses";
 import FirstComponent from "./components/FirstComponents";
 
 const App = () => {
+  const product = [
+    {title:'apple', price:64, date:new Date(2015, 5, 22)},
+    {title:'orange', price:52, date:new Date(2016, 5, 22)},
+    {title:'mango', price:40, date:new Date(2017, 5, 22)}]
+    
   return (
     <div>
       <h1>Let's Start Now</h1>
-      <FirstComponent></FirstComponent>
+      <p className='discription'>Hai, I am a First Component in Your Program</p>
+      <Expanses></Expanses>
     </div>
   );
 }
--- src/components/FirstComponents.js
+++ src/components/FirstComponents.js
@@ -1,5 +1,16 @@
-function FirstComponent() {
-    return <p>Hai, I am a First Component in Your Program</p>
-}
+import './FirstComponents.css'
+import DateExpanses from './ExpansesDate';
+
+function FirstComponent(props) {
+    return (
+        <div className='discription'>
+            <DateExpanses date={props.date}/>
+            <div>
+                <h1>{props.title}</h1>
+                <div>{props.price}</div>
+            </div>
+        </div>
+        )
+};
 
 export default FirstComponent;
