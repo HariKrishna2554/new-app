60bffbddd6e285a3d14c04ca360f90f8d10f3501
hari66mech <harikrishna.manokaran@atmecs.com>
2023-05-03 23:35:40 +0530

    Added the useState

--- src/components/Expenses/ExpensesItem.js
+++ src/components/Expenses/ExpensesItem.js
@@ -1,16 +1,20 @@
+import React, {useState} from 'react';
 import ExpensesDate from './ExpensesDate';
 import Cards from "../UI/Cards";
 import './ExpenseItem.css';
 
 function ExpensesItem(props) {
+    const [title, setTitle] = useState(props.title)
+
     const buttonHandler = () => {
+        setTitle("Saved!");
         alert("Items Details are Saved!");
     };
     return (
         <Cards className='expense-item'>
             <ExpensesDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <Cards className='expense-item__price'>${props.price}</Cards>
             </div>
             <button onClick={buttonHandler}>Save items</button>
